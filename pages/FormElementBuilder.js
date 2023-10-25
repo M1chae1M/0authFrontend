ac65dad014e55db796617b6ff1741c78d56a3d70
M1chae1M <michalmaru10@gmail.com>
2023-10-25 15:14:17 +0200

    new Menu folder structure

--- pages/FormElementBuilder.js
+++ pages/FormElementBuilder.js
@@ -5,7 +5,7 @@ const {Label, Control}=Form
 
 const FormElementBuilder=({name, type, formData, changeV})=>(
     <Element>
-        <Label>{name.charAt(0).toUpperCase()+name.slice(1)}: </Label>
+        <Label>{name?.charAt?.(0)?.toUpperCase?.()+name?.slice?.(1)}: </Label>
         <Control type={type} placeholder={`Enter your ${name}`} name={name} value={formData?.[name]} onChange={changeV}/>
     </Element>
 )

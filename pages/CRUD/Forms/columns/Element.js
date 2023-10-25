531b01d81809cb2849301e1bfe5f18011932704c
M1chae1M <michalmaru10@gmail.com>
2023-10-25 14:08:17 +0200

    new config

--- pages/CRUD/Forms/columns/Element.js
+++ pages/CRUD/Forms/columns/Element.js
@@ -5,14 +5,14 @@ transition:0.2s ease-in-out;
 &:hover >input::placeholder{
     color:#007bff;
 }
-&:hover >input{
+&:hover>input{
     scale:1.01;
     opacity:0.7;
     border:solid #007bff 1.5px;
     color:#007bff;
     font-weight:bold;
 }
-&:hover > label{
+&:hover>label{
     color:#007bff;
     font-weight:bold;
 }`

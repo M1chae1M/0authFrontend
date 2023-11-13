c534e7deecd54e49967d678ea01aa4904f19e91e
M1chae1M <michalmaru10@gmail.com>
2023-11-13 12:48:31 +0100

    english buttons

--- pages/resource_code/GoToRepo.js
+++ pages/resource_code/GoToRepo.js
@@ -3,7 +3,7 @@ import BlueBTN_hover from "../components/BlueBTN_hover"
 const GoToRepo=({href})=>(
     <a href={`https://github.com/M1chae1M/0auth${href}`} target='_blank'>
         <BlueBTN_hover>
-            <button type="button" className="btn btn-primary">Zobacz kod tego projektu</button>
+            <button type="button" className="btn btn-primary">View the project source code</button>
         </BlueBTN_hover>
     </a>
 )

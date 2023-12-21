90f19322851089d9227dd20f65081e446f80e457
M1chae1M <michalmaru10@gmail.com>
2023-12-21 15:20:53 +0100

    url test

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,9 @@ import '../styles/scrollbar.css';
 import appStore from '../STORE/store';
 import {Provider} from 'react-redux';
 
-export const url=CONFIG?.production?CONFIG?.url:'0auth-backend.vercel.app'
+// export const url=CONFIG?.production?CONFIG?.url:'0auth-backend.vercel.app'
 // export const url=CONFIG?.production?CONFIG?.url:'http://localhost:8080'
+export const {url}=CONFIG
 
 const App=({Component, pageProps})=>(
   <Provider store={appStore}>

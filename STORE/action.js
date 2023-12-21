4ce78d36d797368dc8a19d36b6c99b0f9cca4daf
M1chae1M <michalmaru10@gmail.com>
2023-12-21 15:58:35 +0100

    CHANGE_WHERE_LIST reducer

--- STORE/action.js
+++ STORE/action.js
@@ -1,7 +1,6 @@
 import types from './types'
 
 const change_state=(newState)=>({type:types.CHANGE_STATE, ...newState})
-
 const change_where_list=(payload)=>({type:types.CHANGE_WHERE_LIST, payload})
 
 export default {

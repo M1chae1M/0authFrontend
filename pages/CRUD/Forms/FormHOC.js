fd7f0d798f90503acb5b53e6b32ba828c1fd219f
M1chae1M <michalmaru10@gmail.com>
2023-12-21 15:33:45 +0100

    SubmitBTN component

--- pages/CRUD/Forms/FormHOC.js
+++ pages/CRUD/Forms/FormHOC.js
@@ -3,19 +3,12 @@ import SelectForm from "./queryForms/select";
 import InsertForm from "./queryForms/insert";
 import UpdateForm from "./queryForms/update";
 import DeleteForm from "./queryForms/delete";
-import Button from 'react-bootstrap/Button';
 import {Form} from "react-bootstrap";
 import {CRUDPageContext} from "@/pages";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import styled from "styled-components";
 import {connect} from "react-redux";
-
-const SubmitButton=styled(Button)`
-transition:all 0.2s ease-in-out;
-&:hover{
-    transform:scale(1.01);
-}`
+import SubmitButton from "./SubmitButton";
 
 const FormHOC=({formState})=>(
     <CRUDPageContext.Consumer>
@@ -29,7 +22,7 @@ const FormHOC=({formState})=>(
         }
         const Query=allForms?.[formState]
         const QueryForm=Query && <Query/>
-        const button={
+        const style={
             position:'sticky',
             bottom:'0'
         }
@@ -38,7 +31,7 @@ const FormHOC=({formState})=>(
                 <Container>
                     <Row>
                         {QueryForm}
-                        <SubmitButton variant="primary" type="submit" style={button}>submit</SubmitButton>
+                        <SubmitButton variant="primary" type="submit" style={style}>submit</SubmitButton>
                     </Row>
                 </Container>
             </Form>
---
+++ pages/CRUD/Forms/SubmitButton.js
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+import styled from "styled-components";
+
+const SubmitButton=styled(Button)`
+transition:all 0.2s ease-in-out;
+&:hover{
+    transform:scale(1.01);
+}`
+
+export default SubmitButton
